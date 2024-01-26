e537334712e8c064a7500e6e90f88fd6d59d9d81
Shrikant Herwade <Shri.Herwade@gmail.com>
2024-01-26 18:27:33 +0530

    SN-53 Just a workaround to use side menu bar, if we start addgin more tiles .. it start breaking at startup

--- lib/zebra/screen/farming/HomeScreen.dart
+++ lib/zebra/screen/farming/HomeScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sheti_next/zebra/common/widgets/NxNavBar.dart';
+import '../../common/widgets/NavDrawer.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NxNavBar(),
+      drawer: NavDrawer(),
       bottomNavigationBar: GNav(
         backgroundColor: Colors.white,
         color: Colors.black,
