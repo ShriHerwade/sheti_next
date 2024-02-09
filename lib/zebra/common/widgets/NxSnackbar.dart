dab6060eac5f7626b20a7d3e8cef2d1d5ed0910d
Shrikant Herwade <Shri.Herwade@gmail.com>
2024-02-10 00:18:41 +0530

    SN-54 common snackbar introduced, need to use on all the screens

---
+++ lib/zebra/common/widgets/NxSnackbar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:sheti_next/zebra/constant/ColorConstants.dart';
+
+class NxSnackbar {
+  static void showSuccess(BuildContext context, String message,
+      {Duration duration = const Duration(seconds: 2)}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(2.0),
+              decoration: BoxDecoration(
+                color: ColorConstants.snackBarSuccessCircleColor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.check,
+                color: Colors.white,
+                size: 16.0,
+              ),
+            ),
+            SizedBox(width: 6.0),
+            Expanded(
+              child: Text(
+                message,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: ColorConstants.snackBarBackgroundColor,
+        behavior: SnackBarBehavior.floating,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+        ),
+        margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+        duration: duration,
+      ),
+    );
+  }
+
+  static void showError(BuildContext context, String message,
+      {Duration duration = const Duration(seconds: 2)}) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(2.0),
+              decoration: BoxDecoration(
+                color: ColorConstants.snackBarErrorCircleColor,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.close,
+                color: Colors.white,
+                size: 16.0,
+              ),
+            ),
+            SizedBox(width: 6.0),
+            Expanded(
+              child: Text(
+                message,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: ColorConstants.snackBarBackgroundColor,
+        behavior: SnackBarBehavior.floating,
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+        ),
+        margin: EdgeInsets.fromLTRB(50, 10, 50, 10),
+        duration: duration,
+      ),
+    );
+  }
+}
