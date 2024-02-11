79fa2e66117fae938506a1a5268babc052504fc1
Shrikant Herwade <Shri.Herwade@gmail.com>
2024-02-11 18:28:51 +0530

    SN-56 TextField error border fix

--- lib/zebra/common/widgets/NxTextFormField.dart
+++ lib/zebra/common/widgets/NxTextFormField.dart
@@ -8,7 +8,7 @@ class NxTextFormField extends StatefulWidget {
   final String? labelText;
   final IconData? icon;
   final bool isObsecureText;
-  final bool isMandatory ;
+  final bool isMandatory;
   final TextInputType inputType;
   final bool isEnable;
   final int? maxLength;
@@ -16,6 +16,7 @@ class NxTextFormField extends StatefulWidget {
   final bool expands;
   final EdgeInsetsGeometry padding;
   final String? Function(String?)? validator;
+
   const NxTextFormField({
     Key? key,
     this.controller,
@@ -28,10 +29,9 @@ class NxTextFormField extends StatefulWidget {
     this.maxLength,
     this.maxLines,
     this.expands = false,
-    this.isMandatory=true,
+    this.isMandatory = true,
     this.validator,
-    this.padding =
-        const EdgeInsets.symmetric(horizontal: 20.0),  // Default padding
+    this.padding = const EdgeInsets.symmetric(horizontal: 20.0),
   }) : super(key: key);
 
   @override
@@ -45,33 +45,15 @@ class _NxTextFormFieldState extends State<NxTextFormField> {
   void initState() {
     super.initState();
     _customTextFieldController = widget.controller ?? TextEditingController();
-    _customTextFieldController.addListener(updateState);
   }
 
   @override
   void dispose() {
-    _customTextFieldController.removeListener(updateState);
     if (widget.controller == null) {
       _customTextFieldController.dispose();
     }
     super.dispose();
   }
-
-  void updateState() {
-    setState(() {});
-  }
-
- String? validateInput(String? value) {
-    if(widget.isMandatory==true){
-      if (value == null || value.isEmpty) {
-        return "Please Enter ${widget.labelText}";
-      }
-      if (widget.hintText == "Email" && !validateEmail(value)) {
-        return "Please Enter a valid Email Address";
-      }
-      return null;
-    }
-    }
 
   @override
   Widget build(BuildContext context) {
@@ -85,28 +67,42 @@ class _NxTextFormFieldState extends State<NxTextFormField> {
         maxLength: widget.maxLength,
         maxLines: widget.expands ? null : widget.maxLines,
         expands: widget.expands,
-
-       validator: validateInput,
+        onChanged: (value) {
+          setState(() {}); // Update the state to reflect changes
+        },
+        validator: (value) {
+          if (widget.isMandatory && (value == null || value.isEmpty)) {
+            return "Please Enter ${widget.labelText}";
+          }
+          if (widget.hintText == "Email" && !validateEmail(value!)) {
+            return "Please Enter a valid Email Address";
+          }
+          return null;
+        },
         decoration: InputDecoration(
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
             borderSide: BorderSide(
-                width: 1, color: ColorConstants.enabledFieldBorderColor),
+              width: 1, color: ColorConstants.enabledFieldBorderColor,
+            ),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
             borderSide: BorderSide(
-                width: 1, color: ColorConstants.focusedFieldBorderColor),
-          ),
-          disabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            borderSide: BorderSide(
-                width: 1, color: ColorConstants.disabledFieldBorderColor),
+              width: 1,color: ColorConstants.focusedFieldBorderColor,
+            ),
           ),
           errorBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(8.0)),
             borderSide: BorderSide(
-                width: 1, color: ColorConstants.errorFieldBorderColor),
+              width: 1, color: ColorConstants.errorFieldBorderColor,
+            ),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            borderSide: BorderSide(
+              width: 1, color: ColorConstants.errorFieldBorderColor,
+            ),
           ),
           floatingLabelBehavior: FloatingLabelBehavior.auto,
           hintText: widget.hintText,
