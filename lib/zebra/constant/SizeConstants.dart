e014e393a790c8816b0750c56b30e6ad5426d81b
Shrikant Herwade <Shri.Herwade@gmail.com>
2024-03-10 20:14:36 +0530

    SN-62 - list View sizes added, values might need to correct

--- lib/zebra/constant/SizeConstants.dart
+++ lib/zebra/constant/SizeConstants.dart
@@ -1,3 +1,12 @@
 class SizeConstants {
   static const double floatingLabelFontSize=18.0;
+  //listView
+  static const double listViewTitleFontSize=18.0;
+  static const double listViewSubTitleFontSize=18.0;
+  static const double listViewChildFontSize=18.0;
+
+  static const double listViewCardHeight=18.0;
+  static const double listViewSizeBoxBetweenTwoCards=8.0;
+  static const double listViewCardElevation=1.5;
+  static const double listViewCardBorderRadiusCircular=11;
 }
