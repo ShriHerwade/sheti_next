31e4ce395d4bc0a6e23c340120dc7e1c3f09e868
sachin pawar <sachinpawar96@gmail.com>
2024-03-14 23:21:22 +0530

    SN-60 created timeline for task

--- lib/zebra/screen/farming/MyEventTimeline.dart
+++ lib/zebra/screen/farming/MyEventTimeline.dart
@@ -48,7 +48,7 @@ class _MyEventTimelineState extends State<MyEventTimeline> {
             );
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return Center(
-              child: Text("No Task available."),
+              child: Text("No Tasks available."),
             );
           } else {
             // Reverse the order of events
